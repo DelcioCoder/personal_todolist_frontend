b31452c7a95d72015355d7e160a0b6423a097fd5
DelcioCoder <armandodelcio40@gmail.com>
2024-11-15 18:44:21 -0300

    I created the Login component, authentication logic on frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  // Função para autenticar o usuário e armazenar o token JWT
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:8000/user_auth/api/token', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem('token', data.access); // Armazena o token JWT
+        navigate('/activities'); // Redireciona para a página de atividades
+      } else {
+        setError('Login failed. Please check your credentials.');
+      }
+    } catch (error) {
+      setError('An error occurred. Please try again later.');
+    }
+  };
+
   return (
-    <div>
-      <h2>Login page</h2>
+    <div className="login-container">
+      <h2 className='text-center'>Login</h2>
+      <form className='flex-col space-y-6' onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            className='text-black'
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className='text-black'
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </div>
-  )
+  );
 }
+
