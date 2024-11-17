d088948c7fd70dfa3104ec9041ea6596c7febf29
DelcioCoder <armandodelcio40@gmail.com>
2024-11-17 13:20:13 -0300

    I refactored the LoginButton component

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Link as RouterLink } from "react-router-dom";
 
+import { logout } from "../../utils/auth";
+
 export default function LoginButton({ isAuthenticated, isOpen, setIsOpen }) {
+    const handleLogout = () => {
+        logout();
+        setIsOpen(!isOpen); // fecha o menu de navegação
+        window.location.href = "/"; // Redireciona o usuário para a página home
+    }
   return (
     <>
       <motion.div className="bg-blue-600 rounded-xl mx-5 p-2 w-36 text-center transition duration-200 transform hover:scale-105 cursor-pointer">
         {isAuthenticated() ? (
           <RouterLink
             to="/"
-            onClick={() => {
-              localStorage.removeItem("token"); // remove o token do localStorage
-              setIsOpen(!isOpen); // fecha o menu de navegação
-              window.location.href = "/"; // Redireciona o usuário para a página home
-            }}
+            onClick={handleLogout}
             className="text-white font-bold cursor-pointer transition duration-200 hover:opacity-70"
           >
             Logout
